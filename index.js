59a2d63d2b5a600a0a09af5e1c1b8fb642c936b7
Niyom Chamaree <niyomja@hotmail.com>
2020-08-13 13:19:40 +0700

    Update

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const cheerio = require('cheerio');
 const cors = require('cors')
 const server = express()
 const port = process.env.PORT || 5555
-const url = 'https://xn--42cah7d0cxcvbbb9x.com/'
+const url = 'http://asset.led.go.th/newbid/asset_search_province.asp?search_asset_type_id=&search_tumbol=&search_ampur=&search_province=%A1%C3%D8%A7%E0%B7%BE&search_sub_province=&search_price_begin=&search_price_end=&search_bid_date=&search_rai=&search_rai_if=1&search_quaterrai=&search_quaterrai_if=1&search_wa=&search_wa_if=1&search_status=1&search_person1=&search=ok'
 
 server.use(cors())
 
 server.get('/', (req, res) => {
   res.send({
-      status: 'success',
-      response: 'Please go to https://github.com/max180643/thai-gold-api for API usage.',
-    },
+    status: 'success',
+    response: 'Welcome to LED Assets API.',
+  },
     200,
   )
 })
@@ -21,36 +21,20 @@ server.get('/latest', (req, res) => {
   request(url, (error, response, html) => {
     if (!error && response.statusCode === 200) {
       const $ = cheerio.load(html)
-      date = $('#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(5) > td.span.bg-span.txtd.al-r').text()
-      update_time = $('#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(5) > td.em.bg-span.txtd.al-r').text()
-      gold_buy = $('#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(3) > td:nth-child(3)').text()
-      gold_sell = $('#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(3) > td:nth-child(2)').text()
-      goldBar_buy = $('#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(2) > td:nth-child(3)').text()
-      goldBar_sell = $('#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(2) > td:nth-child(2)').text()
+      table = $('table.linkevent > tbody . tr').html()
       res.send({
-          status: 'success',
-          response: {
-            date: date,
-            update_time: update_time,
-            price: {
-              gold: {
-                buy: gold_buy,
-                sell: gold_sell,
-              },
-              gold_bar: {
-                buy: goldBar_buy,
-                sell: goldBar_sell,
-              },
-            }
-          }
-        },
+        status: 'success',
+        response: {
+          'table': table
+        }
+      },
         200,
       )
     } else {
       res.send({
-          status: 'failure',
-          response: 'Service is unavailable, Please try again later.',
-        },
+        status: 'failure',
+        response: 'Service is unavailable, Please try again later.',
+      },
         404,
       )
     }
@@ -59,9 +43,9 @@ server.get('/latest', (req, res) => {
 
 server.get('*', (req, res) => {
   res.send({
-      status: 'failure',
-      response: 'route not found.',
-    },
+    status: 'failure',
+    response: 'route not found.',
+  },
     404,
   )
 })
